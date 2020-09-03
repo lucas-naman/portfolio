06bfefd0833fdba487ad55082fb6ac0a78a786f0
Lucas Naman <lucasnaman@MacBook-Pro-de-Lucas.local>
2020-09-03 13:51:07 +0200

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+ng build --prod --base-href https://lucasnaman.com
+mv dist/portfolio/* dist
+rm -r dist/portfolio
+ngh -no-silent
+echo "Go to"
+echo "https://github.com/lucas-naman/portfolio/settings"
+echo "Check the \"GitHub Pages part\""
+echo "Custom domain section: put \"lucasnaman.com\""
